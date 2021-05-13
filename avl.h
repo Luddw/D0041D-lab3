59fa941b3c0e002edd4507890ee3011c19ee975b
Ludwig Lindmark <carlaxelludwig@hotmail.com>
2021-05-13 18:47:17 +0200

    balancing

--- avl.h
+++ avl.h
@@ -19,14 +19,26 @@ int GetMax(const int a, const int b)
     return (a > b) ? a : b;
 }
 
+
 int GetHeight(TreeNode* node)
 {
-    if (node == nullptr)
+    if (node == NULL)
     {
         return 0;
     }
     return node->depth;
 }
+
+int GetBalance(TreeNode* node)
+{
+    if (node == NULL)
+    {
+        return 0;
+    }
+
+    return GetHeight(node->left) - GetHeight(node->right);
+}
+
 
 class Tree
 {
@@ -34,15 +46,15 @@ private:
     TreeNode* m_root_node;
     size_t m_node_count;
 public:
-    Tree() : m_node_count(0) { m_root_node = nullptr;};
+    Tree() : m_node_count(0) { m_root_node = NULL;};
     ~Tree() {};
 
     TreeNode* CreateNewNode(int key)
     {
         TreeNode* node = new TreeNode();
         node->value = key;
-        node->left = nullptr;
-        node->right = nullptr;
+        node->left = NULL;
+        node->right = NULL;
         node->depth = 1;
 
         return (node);
@@ -55,7 +67,7 @@ public:
             m_root_node = node;
         }
         
-        if (node == nullptr)
+        if (node == NULL)
         {
             return (CreateNewNode(x_key));
         }
@@ -67,20 +79,43 @@ public:
         else
             return node;
 
+        // update depth
         node->depth = 1 + GetMax(GetHeight(node->left), GetHeight(node->right));
+        
+
+        // balance
+        int balance = GetBalance(node);
+
+        // Left Left
+        if (balance > 1 && x_key < node->left->value)
+            return RotateRight(node);
+        // Right Right 
+        if (balance < -1 && x_key > node->right->value)
+            return RotateLeft(node);
+        // Left Right 
+        if (balance > 1 && x_key > node->left->value)
+        {
+            node->left = RotateLeft(node->left);
+            return RotateRight(node);
+        }
+        // Right Left 
+        if (balance < -1 && x_key < node->right->value)
+        {
+            node->right = RotateRight(node->right);
+            return RotateLeft(node);
+        }
         
         m_node_count++;
         return node;
     }
 
+
     void PrintTree(TreeNode* root)
     {
-        if (root != nullptr)
+        if (root != NULL)
         {
             printf("node value %d at depth %d \n", root->value, root->depth);
-            printf("left: ");
             PrintTree(root->left);
-            printf("right: ");
             PrintTree(root->right);
         }
     }
@@ -89,5 +124,47 @@ public:
     {
         return m_root_node;
     }
+
+    TreeNode* RotateLeft(TreeNode* a)
+    {
+        TreeNode* b = a->right;
+        TreeNode* subtree2 = b->left;
+
+        b->left = a;
+        a->right = subtree2;
+
+        // update depth
+        a->depth = GetMax(GetHeight(a->left), GetHeight(b->right)) + 1;
+        b->depth = GetMax(GetHeight(b->left), GetHeight(b->right)) + 1;
+
+        return b;
+    }
+
+    TreeNode* RotateRight(TreeNode* a)
+    {
+        TreeNode* b = a->left;
+        TreeNode* subtree2 = b->right;
+
+        b->right = a;
+        a->right = subtree2;
+
+        // update depth
+        a->depth = GetMax(GetHeight(a->left), GetHeight(b->right)) + 1;
+        b->depth = GetMax(GetHeight(b->left), GetHeight(b->right)) + 1;
+
+        return b;
+    }
+
 };
 
+
+        /* --Rotate
+                 c                               c
+                / \                            /   \ 
+               b   T4  right Rotate (b)        a    T4 
+              / \      - - - - - - - - ->    /  \      
+            T1   a                          b    T3     
+                / \                        / \
+              T2   T3                    T1   T2
+    
+        */
--- main.cc
+++ main.cc
@@ -6,9 +6,56 @@
 
 int main()
 {
-    Tree tree = Tree();
-    TreeNode* node = new TreeNode();
-    node->value = 123123;
-    tree.Insert(node, 123123);
-    tree.PrintTree(tree.GetRoot());
+    Tree tree;
+    TreeNode* node = NULL;
+
+    int command = 0;
+    int val = 0;
+
+    do
+    {
+        std::cout << "\n commands: \n"
+                     "[1] - insert \n"
+                     "[2] - print \n"
+                     "---------------\n"
+                     "enter command: ";
+        std::cin >> command;
+        std::cout << std::endl;
+
+        switch (command)
+        {
+        case 1:
+        {
+            do
+            {
+                printf("Enter number to insert in tree: ");
+                std::cin >> val;
+                
+            } while (val < 1);
+
+            node = tree.Insert(node,val);
+            printf("%d inserted at [%d] \n", node->value, node->depth);
+            break;
+        }
+
+        case 2:
+        {
+            tree.PrintTree(node);
+            break;
+        }
+
+        case 3:
+        {
+
+            break;
+        }
+        default:
+            break;
+        }
+
+    } while (command != 5);
+    
+
+
+
 }
